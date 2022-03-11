9dd08a678ff8c83d5984ff7e13d5593427fa1447
Suryadipta <gremlin.sarkar@gmail.com>
2022-03-11 12:22:39 +0530

    changes

---
+++ src/SocketContext.js
@@ -0,0 +1,101 @@
+import { createContext, useState, useRef, useEffect } from "react";
+
+import {io} from 'socket.io-client'
+import Peer from 'simple-peer'
+
+const SocketContext = createContext()
+
+const socket = io(`https://gremlin-video-chat-test-app.herokuapp.com/`);
+
+const ContextProvider = ({children}) => {
+    const [stream, setStream] = useState(null)
+    const [self, setSelf] = useState('')
+    const [call, setCall] = useState({})
+    const [callAccepted, setCallAccepted] = useState(false)
+    const [callEnded, setCallEnded] = useState(false)
+    const [name, setName] = useState('')
+
+    const myVideo = useRef()
+    const userVideo = useRef()
+    const connectionRef = useRef()
+
+    useEffect(() => {
+        navigator.mediaDevices.getUserMedia({video: true, audio: true})
+        .then((currentStream) => { 
+            setStream(currentStream)
+            myVideo.current.srcObject = currentStream
+        })
+
+        socket.on('self', (id) => setSelf(id))
+
+        socket.on('callUser', ({from, name: callerName, signal}) => {
+            setCall({isReceivingCall: true, from, name: callerName, signal})
+        })
+    }, [])
+
+    const answerCall = () => {
+        setCallAccepted(true)
+
+        const peer = new Peer({ initiator: false, trickle: false, stream })
+
+        peer.on('signal', (data) => {
+            socket.emit('answercall', {signal: data, to: call.from})
+        })
+
+        peer.on('stream', (currentStream) => {
+            userVideo.current.srcObject = currentStream
+        })
+
+        peer.signal(call.signal)
+
+        connectionRef.current = peer
+    }
+
+    const callUser = (id) => {
+        const peer = new Peer({ initiator: true, trickle: false, stream })
+
+        peer.on('signal', (data) => {
+            socket.emit('calluser', {userToCall: id, signalData: data, from: self, name})
+        })
+
+        peer.on('stream', (currentStream) => {
+            userVideo.current.srcObject = currentStream
+        })
+
+        socket.on('callaccepted', (signal) => {
+            setCallAccepted(true)
+
+            peer.signal(signal)
+
+            connectionRef.current = peer
+        })
+
+    }
+
+    const leaveCall = () => {
+        setCallEnded(true)
+        connectionRef.current.destroy()
+        window.location.reload()
+    }
+
+    return(
+        <SocketContext.Provider value={{
+            call,
+            callAccepted,
+            myVideo,
+            userVideo,
+            stream,
+            name,
+            setName,
+            callEnded, 
+            self,
+            callUser,
+            leaveCall,
+            answerCall,
+        }}>
+            {children}
+            </SocketContext.Provider>
+    )
+}
+
+export { ContextProvider, SocketContext }
